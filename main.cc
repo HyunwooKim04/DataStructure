493e5ddd0fcf6adb287ec345b21545545c0f7cc8
HyunwooKim04 <155235090+HyunwooKim04@users.noreply.github.com>
2025-07-08 20:48:55 +0900

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,7 +1,10 @@
 // main.cpp
 #include "stack.h"
+#include "circular_queue.h"
 
 int main() {
+    /* ---stack--- */
+    
     try {
         Stack<int> s(5);
 
@@ -19,5 +22,27 @@ int main() {
         std::cerr << "예외 발생: " << e << "\n";
     }
 
-    return 0;
+    /* ---circular queue--- */
+
+    try {
+        CircularQueue<int> q(5);
+
+        q.Push(10);
+        q.Push(20);
+        q.Push(30);
+
+        std::cout << "큐 내용: " << q << "\n";
+        std::cout << "Front: " << q.Front() << "\n";
+
+        q.Pop();
+        std::cout << "Pop 후 큐 내용: " << q << "\n";
+
+        q.Push(40);
+        q.Push(50);
+        q.Push(60);  // 이 시점에서 꽉 참
+
+        std::cout << "최종 큐: " << q << "\n";
+    } catch (const char* e) {
+        std::cerr << "예외 발생: " << e << "\n";
+    }
 }
